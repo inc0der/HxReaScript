9b7656ef03f963155c3afea4e6a0f3251b8b7982
inc0der <inc0der.dev@gmail.com>
2024-09-13 21:15:05 -0300

    add more gfx bindings and update test file

--- reaper/Graphics.hx
+++ reaper/Graphics.hx
@@ -1,6 +1,6 @@
 package reaper;
 
-import reaper.ReaperTypes;
+import reaper.DrawStrFlag;
 
 @:native("gfx")
 extern class Graphics {
@@ -15,25 +15,87 @@ extern class Graphics {
   public static var a2: Float;
 
   public static var w: Float;
-  public static var h: Float;
-  public static var x: Float;
-  public static var y: Float;
-  public static var clear: Float;
-  public static var texth: Float;
-  
-  @:native("mouse_y")
-  public static var mouseY: Float;
 
-  public static function init(title: String, width: Int, height: Int): Void;
+  public static var h: Float;
+
+  public static var x: Float;
+
+  public static var y: Float;
+
+  public static var mode: Float;
+
+  public static var dest: Float;
+
+  public static var clear: Float;
+
+  @:native("texth")
+  public static var textH: Float;
+
+  @:native("ext_retina")
+  public static var ext_retina : Float;
+  
+  @:native("mouse_x")
+  public static var mouseX  : Float;
+
+  @:native("mouse_y")
+  public static var mouseY  : Float;
+
+  @:native("mouse_wheel")
+  public static var mouseWheel   : Float;
+
+  @:native("mouse_hwheel")
+  public static var mouseHorzWheel   : Float;
+
+  @:native("mouse_cap")
+  public static var mouseCap   : Float;
+
+  public static function init(title: String, ?width: Int, ?height: Int, ?dockState: Bool, ?x: Float, ?y: Float): Void;
+
   @:native("getfont")
   public static function getFont(): Int;
 
+  @:native("getpixel")
   public static function getPixel(): Float;
 
   public static function quit(): Void;
 
   public static function update(): Void;
+  
+  // fx.blit(source[, scale, rotation, srcx, srcy, srcw, srch, destx, desty, destw, desth, rotxoffs, rotyoffs])
+  public static function blit(src: Int, ?scale: Float, ?rotation: Float, ?srcx: Float, ?srcy: Float, ?srcw: Float, ?srch: Float, ?destx: Float, ?desty: Float, ?destw: Float, ?desth: Float, ?rotxoffs: Float, ?rotyoffs: Float): Void;
+
+  // gfx.blitext(source,coordinatelist,rotation)
+  @:native("blitext")
+  public static function blitText(src: Int, coordList: Float, ?rotation: Float): Void;
+
+  @:native("blurto")
+  public static function blurTo(x: Float, y: Float): Void;
 
   public static function arc(x: Float, y: Float, r: Float, startAngle: Float, endAngle: Float, antialias: Bool): Void;
+
   public static function circle(x: Float, y: Float, r: Float, ?fill: Bool, ?antialias: Bool): Void;
+
+  @:native("drawstr")
+  public static function drawString(text: String, ?flags: Int, ?right: Bool, ?bottom: Bool): Void;
+
+  @:native("clienttoscreen")
+  public static function clientToScreen(x: Float, y: Float): Void;
+
+  /**
+   * @TODO Unsure about this method and it's proper types.
+   *
+   * Blits from srcimg(srcx,srcy,srcw,srch) to destination (destx,desty,destw,desth).
+   *  Source texture coordinates are s/t, dsdx represents the change in s coordinate
+   * for each x pixel, dtdy represents the change in t coordinate for each y pixel,
+   * etc. dsdxdy represents the change in dsdx for each line. If usecliprect is
+   * specified and 0, then srcw/srch are ignored. 
+   */
+  @:native("deltablit")
+  public static function deltaBlit(srcImg: String, srcs: Float, srct: Float, srcw: Float, srch: Float, destx: Float, desty: Float, destw: Float, desth: Float, dsdx: Float, dtdx: Float, dsdy: Float, dtdy: Float, dsdxdy: Float, dtdxdy: Float, ?useClipRect: Bool): Void;
+
+  public static function dock(v: Float, ?wx: Float, ?wy: Float, ?ww: Float, ?wh: Float)  : Void;
+
+  @:native("measurestr")
+  public static function measureText(text: String): Float;
+
 }
--- test/Test.hx
+++ test/Test.hx
@@ -1,16 +1,45 @@
 package test;
 
 import reaper.Reaper;
+import reaper.Graphics;
+import reaper.DrawStrFlag;
 
 class Test {
 	static function main() {
 
-    var masterTrack = Reaper.GetMasterTrack(0);
+    var width = 400;
+    var height = 300;
+    var title = "Test Window wit hxReaScript (Lua)";
 
-    if (masterTrack != null) {
-      Reaper.ShowMessageBox("Found master track", "Test", 0);
-    } else {
-      Reaper.ShowMessageBox("Unable to find master track", "Test", 0);
-    }
+    Graphics.init(title, width, height);
+
+    Reaper.defer(Test.update);
 	}
+
+  static function changeColor(r: Float, g: Float, b: Float) {
+    Graphics.r = r;
+    Graphics.g = g;
+    Graphics.b = b;
+  }
+
+  static function drawTextCenter(text: String) {
+    var textW = Graphics.measureText(text);
+    Graphics.x = Graphics.w / 2 - textW / 2;
+    Graphics.y = Graphics.h / 2;
+    Graphics.drawString(text, 0x05);
+  }
+
+  static function drawGraphics() {
+    Graphics.clear = 0x222222;
+    Test.changeColor(0.2, 0.6, 0.2);
+    Graphics.circle(Graphics.w / 2, Graphics.h / 2, 100, true, true);
+    Test.changeColor(0.2, 0.2, 0.2);
+    Test.drawTextCenter("Hello World");
+    Graphics.update();
+  } 
+
+  static function update () {
+    Test.drawGraphics();
+    Reaper.defer(Test.update);
+  }
 }
